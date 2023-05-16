db7cd8c516028620b6ddbec99d3847cfb795ac1e
ssuyash3000 <ssuyash2001@gmail.com>
2023-05-17 00:17:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+import MD5 from "crypto-js/md5"; 
+
+console.log("Working");
+
+
+let date = new Date();
+
+let inputBar = document.getElementById("inputBar");
+
+
+
+function searchInput(text){
+
+    if(text.length != 0){
+        console.log(CryptoJS.MD5("text to hash").toString());
+
+    }
+
+}
+
+
+function handleInput(input){
+    let text = input.value;
+   
+    searchInput(text);
+}
+
+function handleKey(e){
+
+    if(e.target.id === "inputBar"){
+        handleInput(e.target);
+    }
+
+
+}
+
+
+document.addEventListener("keyup", handleKey);
