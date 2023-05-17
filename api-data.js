efa12bcec0bf909ccafaf9d2b058084de8566916
ssuyash3000 <ssuyash2001@gmail.com>
2023-05-17 15:35:29 +0530

    completed first page

--- api-data.js
+++
@@ -1,5 +0,0 @@
-var data = {
-    publicKey: "901e2ea3a1a7a46b81e799ad36384e6f",
-
-    privateKey: "e62834c9bacb28702a03a0715b0108696dc44425",
-}
--- script.js
+++ script.js
@@ -1,24 +1,86 @@
 
-import MD5 from "./node_modules/crypto-js/md5.js"; 
 
-console.log("Working");
+let listOfHeroInDom = document.getElementById("heroList");
+let errorMessage =  document.getElementById("errorMessage");
 
-
-let date = new Date();
 
 let inputBar = document.getElementById("inputBar");
 
+let listOfHeros  = [];
+let favList = [];
+function addHerosToDOM(hero){
+    let li = document.createElement("li");
+  
+    li.innerHTML = 
+    `
+    <img src="${hero.thumbnail.path+"."+hero.thumbnail.extension}" id = "poster"  >
+    <h2 id="heroTitle"  data-id=${hero.id}>${hero.name}</h2>
+    <button id="details" data-id="${hero.id}"> details </button>
+    <button id="favBtn" data-id="${hero.id}" data-title="${hero.name}" >Add to Favourites</button>
+    `
+    listOfHeroInDom.append(li);
 
 
-function searchInput(text){
+}
+
+function renderHeroList(){
+    listOfHeroInDom.innerHTML = "";
+    if(listOfHeros.length === 0){
+        
+  
+        errorMessage.innerHTML = 
+        `No superhero found with that name`;
+      
+        return;
+    }
+    listOfHeroInDom.innerHTML = "";
+    errorMessage.innerHTML = "";
+    for(let i = 0; i < listOfHeros.length; i++){
+        addHerosToDOM(listOfHeros[i]);
+    }
+}
+async function searchInput(text){
 
     if(text.length != 0){
-        console.log(CryptoJS.MD5("text to hash").toString());
+
+        let url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${text}&apikey=c2595c6e10b8e75e6bd3b3c61b14547c&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`;
+        let response = await fetch(url);
+        const data1 = await response.json();
+        listOfHeros = (data1.data.results);
+        console.log(listOfHeros);
+        if(listOfHeros.length === 0){
+          
+            renderHeroList();
+            
+        }else{
+            renderHeroList();
+        }
 
     }
 
 }
-
+function addToFav(heroId, heroTitle){
+    for(i of favList){
+        if(i===heroId){
+            errorMessage.innerHTML = "This hero is already in the Fav List";
+            //clearing out the message in 4 seconds
+            setTimeout(()=>{
+                errorMessage.innerHTML = "";
+            }, 4000);
+            
+            return;
+        }
+    }
+    favList.push(heroId);
+    errorMessage.innerHTML = `${heroTitle} added in the fav List`;
+    //clearing out the message in 4 seconds
+    setTimeout(()=>{
+        errorMessage.innerHTML = "";
+    }, 4000);
+    //updating the favList array in the localStorage each time 
+    // favList is updated 
+    localStorage.setItem("favList", JSON.stringify(favList));
+}
 
 function handleInput(input){
     let text = input.value;
@@ -26,14 +88,27 @@ function handleInput(input){
     searchInput(text);
 }
 
-function handleKey(e){
+function handleKeyAndClick(e){
 
     if(e.target.id === "inputBar"){
         handleInput(e.target);
     }
-
+    if(e.target.id === "details"){
+        let heroId = e.target.dataset.id;
+        localStorage.setItem("heroId", JSON.stringify(heroId));
+        console.log("mealItem Clicked " + mealId + e.target.id);
+        //window.open("./heroDetails/mealDetails.html");
+    }
+    if(e.target.id === "favBtn"){
+        addToFav(e.target.dataset.id, e.target.dataset.title);
+    }
+    if(e.target.id === "favourite"){
+        localStorage.setItem("favHeros", JSON.stringify(favList));
+        //window.open("./Favourite/favourite.html");
+    }
 
 }
 
 
-document.addEventListener("keyup", handleKey);
+document.addEventListener("keyup", handleKeyAndClick);
+document.addEventListener("click", handleKeyAndClick);
